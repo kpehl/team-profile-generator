1eb814a5bd0a82297506b02665207df484658758
Kathleen P <30782087+kpehl@users.noreply.github.com>
2020-10-04 15:08:21 -0400

    manager class created, extending employee class; test verifies that class is correctly

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,15 @@
+const { exportAllDeclaration } = require('@babel/types');
+const { TestScheduler } = require('jest');
+const Manager = require('../lib/Manager');
+
+test('creates a manager class', () => {
+    const manager = new Manager('Rob', '311', 'rob@something.com', 'B1');
+
+    expect(manager.name).toBe('Rob')
+    expect(manager.id).toEqual(expect.any(String));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.role).toEqual('Manager')
+    expect(manager.officeNumber).toEqual(expect.any(String))
+
+    console.log(manager)
+});
